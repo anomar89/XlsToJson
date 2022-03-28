b0f7a8122e1c8f03794604bab0f20e7731d19354
Ramona Selariu <ramona.selariu@bearingpoint.com>
2022-03-28 19:58:46 +0300

    Added the class library project

---
+++ XlsToJson/XlsToJson.cs
@@ -0,0 +1,92 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace XlsToJson
+{
+    public static class XlsToJson
+    {
+        /// <summary>
+        /// Extracts the list of defined names and their associated values from a XLSM file based on the provided regex filtering and returns a JSON with them
+        /// </summary>
+        /// <param name="filePath">The path to the XLSM file that you wish to process</param>
+        /// <param name="filters">An array of regular expressions needed for filtering the defined names. 
+        /// If left null, all the defined names will be included in the JSON object with the exception of the ones from hidden rows or without associated values</param>
+        ///<returns>It returns a nullable string containing the filtered defined names with their associated values in the JSON format</returns>
+        public static string ConvertXlsToJson(string filePath, Regex[]? filters = null)
+        {
+            try
+            {
+                var fieldsValuesPairs = new Dictionary<string, string>();
+
+                var filteredDefinedNames = new Dictionary<string, string>();
+
+                using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(filePath, false))
+                {
+                    var workbookPart = spreadsheetDocument.WorkbookPart;
+
+                    if (workbookPart == null || workbookPart.Workbook == null || workbookPart.Workbook.DefinedNames == null)
+                    {
+                        return string.Empty;
+                    }
+                    DefinedNames definedNames = workbookPart.Workbook.DefinedNames;
+
+                    filteredDefinedNames = XlsToJsonService.FilterDefinedNamesWithRegex(definedNames, filters);
+
+                    var filteredSheets = XlsToJsonService.GetSheetsForFilteredDefinedNames(workbookPart, filteredDefinedNames);
+
+                    if (filteredSheets == null || filteredSheets.Count == 0)
+                    {
+                        return string.Empty;
+                    }
+                    foreach (var sheet in filteredSheets)
+                    {
+                        var sheetData = XlsToJsonService.GetSheetDataByRelationshipId(workbookPart, sheet.Value);
+
+                        if (sheetData == null)
+                        {
+                            continue;
+                        }
+                        foreach (var definedName in filteredDefinedNames.Where(fdn => fdn.Value.Contains(sheet.Key)))
+                        {
+                            if (!definedName.Value.Contains('$'))
+                            {
+                                continue;
+                            }
+                            var columnName = definedName.Value.Split('$', '$')[1];
+
+                            var rowIndex = uint.Parse(definedName.Value[(definedName.Value.LastIndexOf("$") + 1)..]);
+
+                            Cell? cell = XlsToJsonService.GetCell(sheetData, columnName, rowIndex);
+
+                            if (cell == null)
+                            {
+                                continue;
+                            }
+                            var cellValue = XlsToJsonService.GetCellValue(workbookPart, cell);
+
+                            if (definedName.Value.Contains(sheet.Key) && !string.IsNullOrEmpty(cellValue))
+                            {
+                                fieldsValuesPairs.Add(definedName.Key, cellValue);
+                            }
+                        }
+                    }
+                }
+                var bcsJson = JsonConvert.SerializeObject(fieldsValuesPairs);
+
+                return bcsJson;
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ XlsToJson/XlsToJsonService.cs
@@ -0,0 +1,160 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace XlsToJson
+{
+    internal static class XlsToJsonService
+    {
+        internal static Dictionary<string, string> FilterDefinedNamesWithRegex(DefinedNames definedNames, Regex[] filters)
+        {
+            Dictionary<string, string> filteredDefinedNames = new Dictionary<string, string>();
+
+            foreach (DefinedName definedName in definedNames)
+            {
+                if (definedName.Name == null || definedName.Name.Value == null)
+                {
+                    continue;
+                }
+                if (filters == null && !filteredDefinedNames.ContainsKey(definedName.Name.Value))
+                {
+                    filteredDefinedNames.Add(definedName.Name.Value, definedName.InnerText);
+                }
+                else if (filters != null)
+                {
+                    foreach (var filter in filters)
+                    {
+                        if (filter.IsMatch(definedName.Name.Value) && !filteredDefinedNames.ContainsKey(definedName.Name.Value))
+                        {
+                            filteredDefinedNames.Add(definedName.Name.Value, definedName.InnerText);
+                        }
+                    }
+                }
+            }
+            return filteredDefinedNames;
+        }
+
+        internal static string GetCellValue(WorkbookPart workbookPart, Cell cell)
+        {
+            string cellValue = string.Empty;
+
+            if (cell.DataType != null)
+            {
+                if (cell.DataType == CellValues.SharedString)
+                {
+                    if (int.TryParse(cell.InnerText, out int id))
+                    {
+                        SharedStringItem? item = GetSharedStringItemById(workbookPart, id);
+
+                        if (item == null)
+                        {
+                            return cellValue;
+                        }
+                        if (item.Text != null)
+                        {
+                            cellValue = item.Text.Text;
+                        }
+                        else if (item.InnerText != null)
+                        {
+                            cellValue = item.InnerText;
+                        }
+                        else if (item.InnerXml != null)
+                        {
+                            cellValue = item.InnerXml;
+                        }
+                    }
+                }
+            }
+            else if (cell.CellValue != null)
+            {
+                cellValue = cell.CellValue.InnerText;
+            }
+            return cellValue;
+        }
+
+        internal static Dictionary<string, string>? GetSheetsForFilteredDefinedNames(WorkbookPart workbookPart, Dictionary<string, string> filteredDefinedNames)
+        {
+            var sheetsNameRelationshipIdPairs = new Dictionary<string, string>();
+
+            if (workbookPart.Workbook.Sheets != null)
+            {
+                foreach (var sheet in workbookPart.Workbook.Sheets)
+                {
+                    var sheetname = string.Empty;
+
+                    var relationshipId = string.Empty;
+
+                    foreach (OpenXmlAttribute attr in sheet.GetAttributes())
+                    {
+                        if (attr.LocalName == "name")
+                        {
+                            sheetname = attr.Value;
+                        }
+                        if (attr.LocalName == "id")
+                        {
+                            relationshipId = attr.Value;
+                        }
+                    }
+                    foreach (var dn in filteredDefinedNames.Values)
+                    {
+                        if (dn.Contains(sheetname) & !sheetsNameRelationshipIdPairs.ContainsKey(sheetname))
+                        {
+                            sheetsNameRelationshipIdPairs.Add(sheetname, relationshipId);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                return null;
+            }
+            return sheetsNameRelationshipIdPairs;
+        }
+
+        internal static SheetData? GetSheetDataByRelationshipId(WorkbookPart workbookPart, string relationshipId)
+        {
+            SheetData? sheetData = null;
+
+            foreach (var w in workbookPart.WorksheetParts)
+            {
+                string partRelationshipId = workbookPart.GetIdOfPart(w);
+
+                if (partRelationshipId == relationshipId)
+                {
+                    sheetData = w.Worksheet.Elements<SheetData>().First();
+                }
+            }
+            return sheetData;
+        }
+
+        internal static Cell? GetCell(SheetData worksheet, string columnName, uint rowIndex)
+        {
+            Row? row = GetRow(worksheet, rowIndex);
+
+            if (row == null)
+            {
+                return null;
+            }
+            if (row.Hidden != null && row.Hidden.Value == true)
+            {
+                return null;
+            }
+            return row.Elements<Cell>().FirstOrDefault(c => string.Equals(c.CellReference?.Value, columnName + rowIndex, StringComparison.CurrentCultureIgnoreCase));
+
+        }
+
+        internal static Row? GetRow(SheetData worksheet, uint rowIndex)
+        {
+            return worksheet.Elements<Row>().FirstOrDefault(r => r.RowIndex == rowIndex);
+        }
+
+        internal static SharedStringItem? GetSharedStringItemById(WorkbookPart workbookPart, int id)
+        {
+            return workbookPart.SharedStringTablePart?.SharedStringTable.Elements<SharedStringItem>().ElementAt(id);
+        }
+    }
+}
